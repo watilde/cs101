a0e0b53d2f27ee5868f90eb51e2a8f360b9aa3f5
Daijiro Wachi <daijiro.wachi@gmail.com>
2018-01-21 02:43:19 +0100

    add quick sort

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const merge = require('./lib/merge')
+const quick = require('./lib/quick')
 
-const out = merge([1, 5, 3, 9, 6, 4, 8])
+const out = quick([6, 5, 1, 3, 8, 4, 7, 9, 2])
 console.log(out)
