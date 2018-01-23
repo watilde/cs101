d0bac489c2ecf94ed2df023f9f9323a135a5bb4c
Daijiro Wachi <daijiro.wachi@gmail.com>
2018-01-23 23:48:52 +0100

    add pkd and asm

--- network/pkt.c
+++ network/pkt.c
@@ -3,5 +3,18 @@
 #include<sys/socket.h>
 #include<arpa/inet.h>
 
-void main() {
+int main() {
+  int s;
+  if ((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) return -1;
+
+  struct sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = inet_addr("8.8.8.8");
+  addr.sin_port = htons(53);
+
+  if (connect(s, (struct sockaddr *)&addr, sizeof(addr)) == -1) return -1;
+
+  free(s);
+  return 0;
 }
